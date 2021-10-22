fcb0ccd7526e7aa49c50d648d64ee33f7aacc798
Marto_0 <martvanenck1@gmail.com>
2021-10-22 16:33:09 +0200

    changed project infrastructure

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ const express = require('express')
 const app = express()
 const port = 3120
 
-let resultsFile = '../results.json'
+let resultsFile = './results.json'
 
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended:true, limit: '50mb'}));
 
 
-app.use(express.static('../'))
+app.use(express.static('public'))
 
 
 app.post('/log', (req, res) => {
